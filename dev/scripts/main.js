9b2d149888fba3532cd5ab0decfff289dee1ce46
JordanGiustizia <jordangiustizia@gmail.com>
2016-10-25 16:07:06 -0400

    Created File Structure

---
+++ dev/scripts/main.js
@@ -0,0 +1,20 @@
+// GLOBAL VARIABLES //
+var musicmix = {};
+
+// FUNCTIONS //
+
+
+// EVENTS //
+musicmix.events = function() {
+
+};
+
+musicmix.init = function() {
+	// Call Functions
+	musicmix.events();
+};
+
+// DOCUMENT READY //
+$(function() {
+	musicmix.init();
+});
