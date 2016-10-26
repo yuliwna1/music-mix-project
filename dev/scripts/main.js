94740d68ad35f52914f55d17ba85639c4ca66765
Lauren Greenway <lagreenway@gmail.com>
2016-10-26 10:49:32 -0400

    some endpoint work and fixed src error in index.html

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -8,14 +8,19 @@ musicmix.getLyrics = function() {
         url: 'http://api.musixmatch.com/ws/1.1/track.search',
         data: {
             apikey: 'd1f1cb04d0c210368a40509a8dc77f76',
-            q_track: 'Shot For Me',
+            q_lyrics: 'girl love yeah',
             format: 'jsonp',
         },
         dataType: 'jsonp'    
     }).then(function(info) {
-        console.log(info);
+        console.log(info.message.body);
+        var heroLyrics = (info.message.body.track_list[0].track.lyrics_id);
+        var trackID = (info.message.body.track_list[0].track.lyrics_id);
+        console.log(heroLyrics);
     });
 }
+
+//.track.lyrics.get is second endpoint that receives track id
 
 // EVENTS //
 musicmix.events = function() {
