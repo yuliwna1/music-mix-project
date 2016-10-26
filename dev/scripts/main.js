5a62bee6cf7b9ed2026b27f2fd8d6f2f5fb253a3
JordanGiustizia <jordangiustizia@gmail.com>
2016-10-26 14:10:58 -0400

    Added Emoji support

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -48,26 +48,24 @@ THIS FIRST, IT WILL BE GAME OVER FOR YOU, I PROMISE. :) */
 jqueryUI functionality and applies them to elements with a corresponding 
 class. To be called in the init function. */
 
-musicmix.showEmoji = function() {
-    for (var i = 127744; i <= 128591; i++) {
-        // Emoji Index
-        emojiIndex = i.toString();
-
-        // Create a container for the emoji
-        var $emojiContainer = $('<div>');
-        $emojiContainer.addClass('grid-cell emoji-container');
+// DISPLAY EMOJI... TO BE TRIGGERED ON MOUSE CLICK!
+// musicmix.showEmoji = function() {
+//     for (var i = 127744; i <= 128591; i++) {
+//         // Create a container for the emoji
+//         var $emojiContainer = $('<div>');
+//         $emojiContainer.addClass('grid-cell emoji-container');
     
-        // Create The Emoji
-        var $emoji = $('<i>');
-        $emoji.addClass('emoji');
-        $emoji.text('&#' + emojiIndex + ';');
+//         // Create The Emoji
+//         var $emoji = $('<article>');
+//         $emoji.addClass('emoji');
+//         $emoji.html('&#' + i + ';');
 
-        $($emojiContainer).append($emoji);
-        $('html').append($emojiContainer);
+//         $($emojiContainer).append($emoji);
+//         $('html').append($emojiContainer);
 
-        console.log('&#' + emojiIndex + ';')
-    }
-};
+//         console.log('&#' + i + ';')
+//     }
+// };
 
 // *code to be used to make items draggable. May need to adjust class name of draggable item. 
 // **Need to add containment class (area within which user will be allowed to drag item)
